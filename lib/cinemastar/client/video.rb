f8a8859a708d86f7c984553d99ac23cc3e8a40fe
Naoto SHINGAKI <nao.sora+github@gmail.com>
2015-03-08 18:40:46 +0900

    add api result thumbnail

--- lib/cinemastar/client/video.rb
+++ lib/cinemastar/client/video.rb
@@ -3,17 +3,18 @@ require 'json'
 module Cinemastar::Client
   class Video
 
-    attr_reader :title, :path
+    attr_reader :title, :path, :thumbnail
 
     def self.load_json(site, str)
       JSON.parse(str).map! { |f|
-        Video.new(site, f["title"], f["path"])
+        Video.new(site, f["title"], f["path"], f["thumbnail"])
       }
     end
 
-    def initialize(site, title, path)
+    def initialize(site, title, path, thumbnail)
       @title = title
       @path = "#{site}#{path}"
+      @thumbnail = "#{site}/thumbnail/#{thumbnail}"
     end
 
   end
