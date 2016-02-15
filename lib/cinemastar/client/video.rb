c749378ea6119724e726e1fc5de3a63ddfa49612
Naoto SHINGAKI <shingaki@occ.co.jp>
2016-02-15 13:24:06 +0900

    add size

--- lib/cinemastar/client/video.rb
+++ lib/cinemastar/client/video.rb
@@ -4,19 +4,20 @@ require 'time'
 module Cinemastar::Client
   class Video
 
-    attr_reader :title, :path, :thumbnail, :update_at
+    attr_reader :title, :path, :thumbnail, :update_at, :size
 
     def self.load_json(site, str)
       JSON.parse(str).map! { |f|
-        Video.new(site, f["title"], f["path"], f["thumbnail"], f["update_at"]) unless f["update_at"].nil?
+        Video.new(site, f["title"], f["path"], f["thumbnail"], f["update_at"], f["size"] || 0) unless f["update_at"].nil?
       }
     end
 
-    def initialize(site, title, path, thumbnail, update_at)
+    def initialize(site, title, path, thumbnail, update_at, size)
       @title = title
       @path = "#{site}#{path}"
       @thumbnail = "#{site}/thumbnail/#{thumbnail}"
       @update_at = Time.parse(update_at)
+      @size = size
     end
 
     def category
